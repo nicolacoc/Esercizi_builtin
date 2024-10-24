d8f7a4718347483a2711c0491e506a20f990618a
Nicola Cocco <niki@motoimco.it>
2024-10-24 15:34:26 +0200

    Revert "New Commit"
    This reverts commit e1ae80f528033bfb8bd3cc500fe45d331e438aff.

--- Date/Esercizio_1.php
+++
@@ -1,7 +0,0 @@
-<?php
-function copyright(string $name):string{
-    return "Copyright ".date("Y")." &copy;".$name;
-}
-
-echo copyright("MYCOMPANY s.r.l..");
-
--- Date/Esercizio_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$type = array(DateTimeInterface::ATOM, DateTimeInterface::ISO8601_EXPANDED, DateTimeInterface::RFC822, DateTimeInterface::RFC850, DateTimeInterface::RFC1036);
-
-
-foreach ($type as $value) {
-    $date = date($value);
-    echo $date . PHP_EOL;
-}
